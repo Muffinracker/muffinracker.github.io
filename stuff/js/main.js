349bda4c99c61022562273cb5e9ac3bc24065cd9
Arda <96730348+Muffinracker342@users.noreply.github.com>
2022-02-12 17:10:58 +0100

    Dont wake the Wombat
    My first webiste

---
+++ stuff/js/main.js
@@ -0,0 +1,19 @@
+//der Wombat ist ein schnelles Tier!
+
+
+var AlertText1 = "Der Wombat Lebt in Australien!";
+
+function Cluck() {
+
+    alert(AlertText1); 
+}
+
+
+var AlertText2 = "Der Wombat ist 40kmh schnell";
+
+function Onclick() {
+    
+    alert(AlertText2);
+}
+
+
